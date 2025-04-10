5d9b7b83f26953927956eab5add18a6b98711abe
Douglas Dreer <douglasdreer@outlook.com.br>
2025-04-10 15:18:40 -0300

    [FEATURE] Criação da classe BaseMapper para conversão de objetos e manipulação de JSON

---
+++ src/main/java/br/com/douglasdreer/the_barbers_forge/dtos/mappers/BaseMapper.java
@@ -0,0 +1,94 @@
+package br.com.douglasdreer.the_barbers_forge.dtos.mappers;
+
+import br.com.douglasdreer.the_barbers_forge.exceptions.ConverterException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.modelmapper.ModelMapper;
+
+import java.io.IOException;
+import java.util.List;
+
+public class BaseMapper {
+    private final ModelMapper modelMapper;
+    private final ObjectMapper objectMapper;
+
+    /**
+     * Constructor for initializing the service with the required dependencies.
+     *
+     * @param modelMapper the ModelMapper instance for object-to-object conversion
+     */
+    public BaseMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+        this.objectMapper = initializeObjectMapper();
+    }
+
+    /**
+     * Initializes and configures the {@link ObjectMapper} for JSON operations.
+     * Enables pretty printing and registers the JavaTimeModule for handling Java 8 date/time types.
+     *
+     * @return a configured ObjectMapper instance
+     */
+    private ObjectMapper initializeObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return mapper;
+    }
+
+    /**
+     * Converts an object of one type to another using {@link ModelMapper}.
+     *
+     * @param source the object to be converted
+     * @param targetClass the class type to convert to
+     * @param <D> the target type
+     * @return the converted object of type {@link D}
+     */
+    public <D> D convertTo(Object source, Class<D> targetClass) {
+        try {
+            return modelMapper.map(source, targetClass);
+        } catch (Exception e) {
+            throw new ConverterException(e.getLocalizedMessage());
+        }
+    }
+
+    /**
+     * Maps a list of objects from one type to another.
+     *
+     * @param sourceList the list of objects to be mapped
+     * @param targetClass the class type to convert each element to
+     * @param <S> the source type
+     * @param <T> the target type
+     * @return a list of objects of type {@link T}
+     */
+    public <S, T> List<T> mapList(List<S> sourceList, Class<T> targetClass) {
+        return sourceList.stream()
+                .map(element -> modelMapper.map(element, targetClass))
+                .toList();
+    }
+
+    /**
+     * Converts an object to its JSON string representation.
+     *
+     * @param object the object to be converted to JSON
+     * @return the JSON string representation of the object
+     * @throws JsonProcessingException if the object cannot be converted to JSON
+     */
+    public String toJSON(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
+
+    /**
+     * Converts a JSON string to an object of the specified type.
+     *
+     * @param jsonData the JSON string to be converted
+     * @param targetClass the class type to convert the JSON string to
+     * @param <D> the target type
+     * @return the object converted from the JSON string
+     * @throws IOException if there is an issue during the deserialization
+     */
+    public <D> D toObject(String jsonData, Class<D> targetClass) throws IOException {
+        return objectMapper.readValue(jsonData, targetClass);
+    }
+}
