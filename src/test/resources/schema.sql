be8cc5235177d90533a7c0213812f19929d8383b
Douglas Dreer <douglasdreer@outlook.com.br>
2025-04-10 16:05:45 -0300

    [FEATURE] Criação de testes de integração para o DocumentService, incluindo operações CRUD e validações de exceções.

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,5 +1,8 @@
 -- Create table if not exist for integration test.
+DROP TABLE IF EXISTS  TBL0003_CUSTOMERS_CONTACTS;
+DROP TABLE IF EXISTS  TBL0002_DOCUMENTS;
 DROP TABLE IF EXISTS  TBL0001_CUSTOMERS;
+
 CREATE TABLE TBL0001_CUSTOMERS (
                                                  id BIGINT AUTO_INCREMENT PRIMARY KEY,
                                                  first_name VARCHAR(255) NOT NULL,
@@ -10,4 +13,20 @@ CREATE TABLE TBL0001_CUSTOMERS (
                                                  birth_date DATE,
                                                  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
                                                  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE TBL0002_DOCUMENTS (
+                                               id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                               document_type VARCHAR(50) NOT NULL,
+                                               number VARCHAR(50) NOT NULL,
+                                               created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                                               updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE TBL0003_CUSTOMERS_CONTACTS (
+                                            customer_id BIGINT NOT NULL,
+                                            document_id BIGINT NOT NULL,
+                                            PRIMARY KEY (customer_id, document_id),
+                                            FOREIGN KEY (customer_id) REFERENCES TBL0001_CUSTOMERS(id),
+                                            FOREIGN KEY (document_id) REFERENCES TBL0002_DOCUMENTS(id)
 );
