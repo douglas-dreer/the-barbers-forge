7b4f029e3161ccbd1cb86b01649c9462dbecc9c9
Douglas Dreer <douglasdreer@outlook.com.br>
2025-04-17 11:09:27 -0300

    Add an overloaded constructor to the ConverterException class
    Introduced a new constructor to handle exceptions caused by JSON processing. This enhances flexibility in exception handling for scenarios involving JSON-related errors.

--- src/main/java/br/com/douglasdreer/the_barbers_forge/exceptions/ConverterException.java
+++ src/main/java/br/com/douglasdreer/the_barbers_forge/exceptions/ConverterException.java
@@ -1,7 +1,12 @@
 package br.com.douglasdreer.the_barbers_forge.exceptions;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public class ConverterException extends RuntimeException {
     public ConverterException(String message) {
         super(message);
+    }
+
+    public ConverterException(String message, JsonProcessingException e) {
     }
 }
